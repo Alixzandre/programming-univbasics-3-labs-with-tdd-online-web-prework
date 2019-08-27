a16f91fa48e7ba9f62b3eb77c4749d18832488fc
Alexander Bispo <alexbispo173@gmail.com>
2019-08-27 01:22:55 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,5 @@
 first_number = 2.0
 second_number = 2.0
 sum = 4.0
-difference = "first_number-second_number"
-quotient = "first_number/second_number"
-product = "first_number*second_number"
+difference = 0.0
+quotient = 1.0
